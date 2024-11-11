29b80969d241cdde26a046f02fad7b9982ecb86b
ramprasathmk <mkramprasath12@gmail.com>
2024-11-11 15:44:59 +0545

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -7,6 +7,7 @@ CREATE TABLE users (
     phone VARCHAR(15) NOT NULL,
     password VARCHAR(255) NOT NULL,
     user_type VARCHAR(10) DEFAULT 'employee',  -- Using VARCHAR for user_type
+    is_admin BOOLEAN DEFAULT FALSE
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Create a table for user roles
